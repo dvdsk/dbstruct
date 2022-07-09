d97360ba9c254df834988cb910e8f9037a9c3aa8
dvdsk <noreply@davidsk.dev>
2022-07-10 01:18:31 +0200

    DataStore key and value types can be different

--- src/traits.rs
+++ src/traits.rs
@@ -1,22 +1,22 @@
 use core::fmt;
 use serde::{de::DeserializeOwned, Serialize};
 
-pub trait DataStore<T> {
+pub trait DataStore<K, V>
+where
+    K: Serialize + DeserializeOwned,
+    V: Serialize + DeserializeOwned,
+{
     type Error: fmt::Debug;
-    fn get(&self, key: &T) -> Result<T, Self::Error>;
-    fn insert(&self, key: T, val: T) -> Result<(), Self::Error>;
-    fn atomic_update(
-        &self,
-        key: T,
-        op: impl FnMut(T) -> T + Clone,
-    ) -> Result<(), Self::Error>;
-    fn conditional_update(&self, key: T, new: T, expected: T) -> Result<(), Self::Error>;
+    fn get(&self, key: &K) -> Result<V, Self::Error>;
+    fn insert(&self, key: K, val: V) -> Result<Option<V>, Self::Error>;
+    fn atomic_update(&self, key: K, op: impl FnMut(V) -> V + Clone) -> Result<(), Self::Error>;
+    fn conditional_update(&self, key: K, new: V, expected: V) -> Result<(), Self::Error>;
 }
 
-trait BytesStore<T> {
+trait BytesStore {
     type Error: fmt::Debug;
     fn get(&self, key: &[u8]) -> Result<Vec<u8>, Self::Error>;
-    fn insert(&self, key: &[u8], val: &[u8]) -> Result<(), Self::Error>;
+    fn insert(&self, key: &[u8], val: &[u8]) -> Result<Option<Vec<u8>>, Self::Error>;
     fn atomic_update(
         &self,
         key: &[u8],
@@ -30,43 +30,43 @@ trait BytesStore<T> {
     ) -> Result<(), Self::Error>;
 }
 
-impl<T, E, BS> DataStore<T> for BS 
+impl<K, V, E, BS> DataStore<K, V> for BS
 where
+    K: Serialize + DeserializeOwned,
+    V: Serialize + DeserializeOwned,
     E: fmt::Debug,
-    T: Serialize + DeserializeOwned,
-    BS: BytesStore<T, Error = E>,
+    BS: BytesStore<Error = E>,
 {
     type Error = E;
 
-    fn get(&self, key: &T) -> Result<T, Self::Error> {
+    fn get(&self, key: &K) -> Result<V, Self::Error> {
         let key = bincode::serialize(key).unwrap();
         let val = BS::get(&self, &key).unwrap();
         let val = bincode::deserialize(&val).unwrap();
         Ok(val)
     }
 
-    fn insert(&self, key: T, val: T) -> Result<(), Self::Error> {
+    fn insert(&self, key: K, val: V) -> Result<Option<V>, Self::Error> {
         let key = bincode::serialize(&key).unwrap();
         let val = bincode::serialize(&val).unwrap();
-        BS::insert(&self, &key, &val).unwrap();
-        Ok(())
+        let existing = BS::insert(&self, &key, &val).unwrap();
+        Ok(match existing {
+            Some(bytes) => bincode::deserialize(&bytes).unwrap(),
+            None => None,
+        })
     }
 
-    fn atomic_update(
-        &self,
-        key: T,
-        mut op: impl FnMut(T) -> T + Clone,
-    ) -> Result<(), Self::Error> {
+    fn atomic_update(&self, key: K, mut op: impl FnMut(V) -> V + Clone) -> Result<(), Self::Error> {
         let key = bincode::serialize(&key).unwrap();
         let bytes_op = |old: &[u8]| -> Vec<u8> {
-            let old: T =  bincode::deserialize(old).unwrap(); // TODO error handling
+            let old: V = bincode::deserialize(old).unwrap(); // TODO error handling
             let new = op(old);
             bincode::serialize(&new).unwrap()
         };
         BS::atomic_update(&self, &key, bytes_op)
     }
 
-    fn conditional_update(&self, key: T, new: T, expected: T) -> Result<(), Self::Error> {
+    fn conditional_update(&self, key: K, new: V, expected: V) -> Result<(), Self::Error> {
         let key = bincode::serialize(&key).unwrap();
         let new = bincode::serialize(&new).unwrap();
         let expected = bincode::serialize(&expected).unwrap();
