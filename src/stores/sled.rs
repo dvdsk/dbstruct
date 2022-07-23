9106742510fa7241ac17217fe58b38d7ce29a5a7
dvdsk <noreply@davidsk.dev>
2022-07-23 17:45:41 +0200

    fix error handling in generic DataStore impl

--- src/stores/sled.rs
+++ src/stores/sled.rs
@@ -1,3 +1,92 @@
+use serde::{de::DeserializeOwned, Serialize};
+
+use crate::traits::DataStore;
 
 pub struct Sled {
+    tree: sled::Tree,
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("value could not be deserialized using bincode")]
+    DeSerializing(bincode::Error),
+    #[error("value could not be serialized using bincode")]
+    SerializingValue(bincode::Error),
+    #[error("could not serialize key using bincode")]
+    SerializingKey(bincode::Error),
+    #[error("the database returned an error")]
+    Database(#[from] sled::Error),
+}
+
+impl<K, V> DataStore<K, V> for Sled
+where
+    K: Serialize,
+    V: Serialize + DeserializeOwned,
+{
+    type Error = Error;
+
+    fn get(&self, key: &K) -> Result<Option<V>, Self::Error> {
+        let key = bincode::serialize(key).map_err(Error::SerializingKey)?;
+        match self.tree.get(key)? {
+            Some(bytes) => Ok(bincode::deserialize(&bytes).map_err(Error::DeSerializing)?),
+            None => Ok(None),
+        }
+    }
+
+    fn remove<'a>(&self, key: &'a K) -> Result<Option<V>, Self::Error> {
+        let key = bincode::serialize(key).map_err(Error::SerializingKey)?;
+        let existing = self.tree.remove(&key)?;
+        match existing {
+            None => Ok(None),
+            Some(bytes) => {
+                let val = bincode::deserialize(&bytes).map_err(Error::DeSerializing)?;
+                Ok(Some(val))
+            }
+        }
+    }
+
+    fn insert<'a>(&self, key: &'a K, val: &'a V) -> Result<Option<V>, Self::Error> {
+        let key = bincode::serialize(key).map_err(Error::SerializingKey)?;
+        let val = bincode::serialize(val).map_err(Error::SerializingValue)?;
+        let existing = self.tree.insert(key, val)?;
+
+        match existing {
+            None => Ok(None),
+            Some(bytes) => {
+                let val = bincode::deserialize(&bytes).map_err(Error::DeSerializing)?;
+                Ok(Some(val))
+            }
+        }
+    }
+
+    fn atomic_update(&self, key: &K, op: impl FnMut(V) -> V + Clone) -> Result<(), Self::Error> {
+        let mut res = Ok(());
+        let update = |old: Option<&[u8]>| match old {
+            None => None,
+            Some(old) => match bincode::deserialize(old) {
+                Err(e) => {
+                    res = Err(Error::DeSerializing(e));
+                    Some(old.to_vec())
+                }
+                Ok(v) => {
+                    let new = op.clone()(v);
+                    match bincode::serialize(&new) {
+                        Ok(new_bytes) => Some(new_bytes),
+                        Err(e) => {
+                            res = Err(Error::SerializingValue(e));
+                            Some(old.to_vec())
+                        }
+                    }
+                }
+            },
+        };
+
+        let key = bincode::serialize(key).map_err(Error::SerializingKey)?;
+        self.tree.update_and_fetch(self.key, update)?;
+        Ok(())
+    }
+
+    fn conditional_update(&self, key: &K, new: &V, expected: &V) -> Result<(), Self::Error> {
+        todo!()
+    }
 }
--- src/traits.rs
+++ src/traits.rs
@@ -10,6 +10,7 @@ where
     fn get(&self, key: &K) -> Result<Option<V>, Self::Error>;
     fn remove<'a>(&self, key: &'a K) -> Result<Option<V>, Self::Error>;
     fn insert<'a>(&self, key: &'a K, val: &'a V) -> Result<Option<V>, Self::Error>;
+    /// on error the update is aborted
     fn atomic_update(&self, key: &K, op: impl FnMut(V) -> V + Clone) -> Result<(), Self::Error>;
     fn conditional_update(&self, key: &K, new: &V, expected: &V) -> Result<(), Self::Error>;
 }
@@ -32,6 +33,18 @@ trait BytesStore {
     ) -> Result<(), Self::Error>;
 }
 
+#[derive(Debug, thiserror::Error)]
+pub enum Error<DbError: fmt::Debug> {
+    #[error("value could not be deserialized using bincode")]
+    DeSerializing(bincode::Error),
+    #[error("value could not be serialized using bincode")]
+    SerializingValue(bincode::Error),
+    #[error("could not serialize key using bincode")]
+    SerializingKey(bincode::Error),
+    #[error("the database returned an error")]
+    Database(#[from] DbError),
+}
+
 impl<K, V, E, BS> DataStore<K, V> for BS
 where
     K: Serialize + DeserializeOwned,
@@ -39,51 +52,70 @@ where
     E: fmt::Debug,
     BS: BytesStore<Error = E>,
 {
-    type Error = E;
+    type Error = Error<E>;
 
     fn get(&self, key: &K) -> Result<Option<V>, Self::Error> {
-        let key = bincode::serialize(key).unwrap();
-        let val = BS::get(&self, &key).unwrap();
+        let key = bincode::serialize(key).map_err(Error::SerializingKey)?;
+        let val = BS::get(&self, &key)?;
         Ok(match val {
-            Some(bytes) => bincode::deserialize(&bytes).unwrap(),
+            Some(bytes) => bincode::deserialize(&bytes).map_err(Error::DeSerializing)?,
             None => None,
         })
     }
 
     fn remove(&self, key: &K) -> Result<Option<V>, Self::Error> {
-        let key = bincode::serialize(key).unwrap();
-        let val = BS::remove(&self, &key).unwrap();
+        let key = bincode::serialize(key).map_err(Error::SerializingKey)?;
+        let val = BS::remove(&self, &key)?;
         Ok(match val {
-            Some(bytes) => bincode::deserialize(&bytes).unwrap(),
+            Some(bytes) => bincode::deserialize(&bytes).map_err(Error::DeSerializing)?,
             None => None,
         })
     }
 
     fn insert(&self, key: &K, val: &V) -> Result<Option<V>, Self::Error> {
-        let key = bincode::serialize(key).unwrap();
-        let val = bincode::serialize(val).unwrap();
-        let existing = BS::insert(&self, &key, &val).unwrap();
+        let key = bincode::serialize(key).map_err(Error::SerializingKey)?;
+        let val = bincode::serialize(val).map_err(Error::SerializingValue)?;
+        let existing = BS::insert(&self, &key, &val)?;
         Ok(match existing {
-            Some(bytes) => bincode::deserialize(&bytes).unwrap(),
+            Some(bytes) => bincode::deserialize(&bytes).map_err(Error::DeSerializing)?,
             None => None,
         })
     }
 
-    fn atomic_update(&self, key: &K, mut op: impl FnMut(V) -> V + Clone) -> Result<(), Self::Error> {
-        let key = bincode::serialize(key).unwrap();
+    fn atomic_update(
+        &self,
+        key: &K,
+        mut op: impl FnMut(V) -> V + Clone,
+    ) -> Result<(), Self::Error> {
+        let key = bincode::serialize(key).map_err(Error::SerializingKey)?;
+        let mut res = Ok(());
         let bytes_op = |old: &[u8]| -> Vec<u8> {
-            let old: V = bincode::deserialize(old).unwrap(); // TODO error handling
-            let new = op(old);
-            bincode::serialize(&new).unwrap()
+            match bincode::deserialize(old) {
+                Err(e) => {
+                    res = Err(Error::DeSerializing(e));
+                    old.to_vec()
+                }
+                Ok(val) => {
+                    let new = op(val);
+                    match bincode::serialize(&new) {
+                        Err(e) => {
+                            res = Err(Error::DeSerializing(e));
+                            old.to_vec()
+                        }
+                        Ok(new_bytes) => new_bytes,
+                    }
+                }
+            }
         };
-        BS::atomic_update(&self, &key, bytes_op)
+        BS::atomic_update(&self, &key, bytes_op)?;
+        res
     }
 
     fn conditional_update(&self, key: &K, new: &V, expected: &V) -> Result<(), Self::Error> {
-        let key = bincode::serialize(key).unwrap();
-        let new = bincode::serialize(new).unwrap();
-        let expected = bincode::serialize(expected).unwrap();
-        BS::conditional_update(&self, &key, &new, &expected).unwrap();
+        let key = bincode::serialize(key).map_err(Error::SerializingKey)?;
+        let new = bincode::serialize(new).map_err(Error::SerializingValue)?;
+        let expected = bincode::serialize(expected).map_err(Error::SerializingValue)?;
+        BS::conditional_update(&self, &key, &new, &expected)?;
         Ok(())
     }
 }
