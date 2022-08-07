42ecae7170b47fe14981888e653a402e4c7b8e22
dvdsk <noreply@davidsk.dev>
2022-08-07 12:40:30 +0200

    add default trait example

---
+++ tests/default_trait.rs
@@ -0,0 +1,27 @@
+use std::collections::HashMap;
+
+#[dbstruct::dbstruct]
+pub struct Test {
+    /// a small list that we dont want structdb to wrap for us
+    #[dbstruct(Default)]
+    small_list: Vec<u8>,
+    /// a small list that we dont want structdb to wrap for us
+    #[dbstruct(Default)]
+    small_map: HashMap<usize, u32>,
+}
+
+fn main() {
+    let ds = dbstruct::stores::HashMap::default();
+    let db = Test::new(ds).unwrap();
+
+    assert!(db.small_list().get().unwrap().is_empty());
+    assert!(db.small_map().get().unwrap().is_empty());
+
+    let list = vec![1,2,3,4];
+    db.small_list().set(&list).unwrap();
+    assert_eq!(list, db.small_list().get().unwrap());
+
+    let map: HashMap<_,_> = (4..8).into_iter().enumerate().collect();
+    db.small_map().set(&map).unwrap();
+    assert_eq!(map, db.small_map().get().unwrap());
+}
