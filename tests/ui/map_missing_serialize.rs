a5a0193c9dba76cb216eca96ec6da4de067af8be
dvdsk <noreply@davidsk.dev>
2025-01-31 14:14:16 +0100

    update ui tests

--- tests/ui/map_missing_serialize.rs
+++ tests/ui/map_missing_serialize.rs
@@ -4,7 +4,8 @@ struct CustomValType;
 
 fn main() {
     // ONCE the output changes and the span starts highlighting the 
-    // key and value type in the struct we can close: https://github.com/dvdsk/dbstruct/issues/13
+    // key and value type in the struct we can close: 
+    // https://github.com/dvdsk/dbstruct/issues/13
 
     #[dbstruct::dbstruct(db=sled)]
     struct PersistentData {
